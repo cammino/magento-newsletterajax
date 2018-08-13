160a7598324f0067ccd01c6ff057906f1f2d59c4
Marcio Junior <marciofmjr@hotmail.com>
2018-08-13 10:46:01 -0300

    #9556 - Corrige cadastro da newsletter no footer

--- controllers/SubscriberController.php
+++ controllers/SubscriberController.php
@@ -5,6 +5,7 @@ class Cammino_Newsletterajax_SubscriberController extends Mage_Newsletter_Subscr
     public function newAction()
     {
         $response = array();
+        
         if ($this->getRequest()->isPost() && $this->getRequest()->getPost('email')) {
             $session            = Mage::getSingleton('core/session');
             $customerSession    = Mage::getSingleton('customer/session');
@@ -14,11 +15,11 @@ class Cammino_Newsletterajax_SubscriberController extends Mage_Newsletter_Subscr
                 $message = $this->__('Please enter a valid email address.');
                 $response['status'] = 'ERROR';
                 $response['message'] = $message;
-            }else if (Mage::getStoreConfig(Mage_Newsletter_Model_Subscriber::XML_PATH_ALLOW_GUEST_SUBSCRIBE_FLAG) != 1 && !$customerSession->isLoggedIn()) {
+            } else if (Mage::getStoreConfig(Mage_Newsletter_Model_Subscriber::XML_PATH_ALLOW_GUEST_SUBSCRIBE_FLAG) != 1 && !$customerSession->isLoggedIn()) {
                 $message = $this->__('Sorry, but administrator denied subscription for guests. Please <a href="%s">register</a>.', Mage::helper('customer')->getRegisterUrl());
                 $response['status'] = 'ERROR';
                 $response['message'] = $message;
-            }else{
+            } else {
                 $ownerId = Mage::getModel('customer/customer')
                     ->setWebsiteId(Mage::app()->getStore()->getWebsiteId())
                     ->loadByEmail($email)
@@ -28,18 +29,19 @@ class Cammino_Newsletterajax_SubscriberController extends Mage_Newsletter_Subscr
                     $message = $this->__('This email address is already assigned to another user.');
                     $response['status'] = 'WARNING';
                     $response['message'] = $message;
-                }else if ($status == Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE) {
+                } else if ($status == Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE) {
                     $status = Mage::getModel('newsletter/subscriber')->subscribe($email);
                     $message = $this->__('Confirmation request has been sent.');
                     $response['status'] = 'SUCCESS';
                     $response['message'] = $message;
                 } else {
+                    Mage::getModel('newsletter/subscriber')->subscribe($email);
                     $message = $this->__('Thank you for your subscription.');
                     $response['status'] = 'SUCCESS';
                     $response['message'] = $message;
                 }
             }
-        }else{
+        } else {
             $message = "Não é um post, ou não existe o campo email";
             $response['status'] = 'ERROR';
             $response['message'] = $message;
